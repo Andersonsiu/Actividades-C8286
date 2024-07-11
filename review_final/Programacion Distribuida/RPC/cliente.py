0553b7c289e1dd378359de32531a38cb1b653b39
Andersonsiu <118629735+Andersonsiu@users.noreply.github.com>
2024-07-11 00:56:55 -0500

    Update cliente.py

--- review_final/Programacion Distribuida/RPC/cliente.py
+++ review_final/Programacion Distribuida/RPC/cliente.py
@@ -6,16 +6,18 @@ def run():
     with grpc.insecure_channel('localhost:50051') as channel:
         stub = analisis_datos_pb2_grpc.AnalisisServiceStub(channel)
 
+        metadata = [('authorization', 'valid_token')]
+
         # Prueba del método Agregar
-        response = stub.Agregar(analisis_datos_pb2.AgregarRequest(numeros=[1, 2, 3, 4, 5]))
+        response = stub.Agregar(analisis_datos_pb2.AgregarRequest(numeros=[1, 2, 3, 4, 5]), metadata=metadata)
         print(f"Resultado de la suma: {response.resultado}")
 
         # Prueba del método Filtrar
-        response = stub.Filtrar(analisis_datos_pb2.FiltrarRequest(numeros=[1, 2, 3, 4, 5], umbral=3))
+        response = stub.Filtrar(analisis_datos_pb2.FiltrarRequest(numeros=[1, 2, 3, 4, 5], umbral=3), metadata=metadata)
         print(f"Resultado del filtrado: {response.resultado}")
 
         # Prueba del método Transformar
-        response = stub.Transformar(analisis_datos_pb2.TransformarRequest(numeros=[1, 2, 3, 4, 5], operacion='doble'))
+        response = stub.Transformar(analisis_datos_pb2.TransformarRequest(numeros=[1, 2, 3, 4, 5], operacion='doble'), metadata=metadata)
         print(f"Resultado de la transformación (doble): {response.resultado}")
 
 if __name__ == '__main__':
